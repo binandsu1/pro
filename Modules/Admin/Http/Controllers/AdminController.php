919fe54b06dfec58a3e369272d2da39e730584a4
闫小君 <252828692@qq.com>
2020-12-02 10:32:01 +0800

    xd

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -3,27 +3,23 @@
 namespace Modules\Admin\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Artisan;
-use Laravel\Fortify\Contracts\LoginResponse;
 use Modules\Laravel\Models\XdoArtisan;
 use Modules\Laravel\Models\XdoEnv;
 use PasswordValidationRules;
-use Laravel\Fortify\Rules\Password;
+
 //class AdminController extends Controller
 class AdminController extends \App\Http\Controllers\AdminController
 {
 
-
-
     protected $redirectTo = '/admin';
+
     public function login(Request $request)
     {
-        if($request->method() == 'POST'){
+        if ($request->method() == 'POST') {
             #显示一个基本的验证
             $this->validateLogin($request);
             if ($this->attemptLogin($request)) {
@@ -39,7 +35,7 @@ class AdminController extends \App\Http\Controllers\AdminController
         $request->validate([
             'name' => 'required|string|min:3|max:10',
             'password' => 'required|string|min:3|max:10',
-        ],[
+        ], [
             'name.required' => '请输入用户名',
             'name.min' => '用户名最少3字符',
             'name.max' => '用户名最多10字符',
@@ -56,7 +52,7 @@ class AdminController extends \App\Http\Controllers\AdminController
 //        $data['status'] = 0;
 //        $data['hr_status'] = 'A';
         // 验证登录的逻辑
-        $result = $this->auth->attempt(
+         $this->auth->attempt(
             $data,
             $request->filled('remember')
         );
@@ -67,14 +63,12 @@ class AdminController extends \App\Http\Controllers\AdminController
     protected function sendLoginResponse(Request $request)
     {
         $request->session()->regenerate();
-
         return $this->authenticated($request, $this->auth->user())
             ?: redirect()->intended($this->redirectPath());
     }
 
     protected function authenticated(Request $request, $user)
     {
-        //
     }
 
     protected function sendFailedLoginResponse(Request $request)
@@ -89,36 +83,30 @@ class AdminController extends \App\Http\Controllers\AdminController
         if (method_exists($this, 'redirectTo')) {
             return $this->redirectTo();
         }
-        $result =  property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
+        $result = property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
         return $result;
     }
 
 
-    public function searchGlobal(Request $request){
-
+    public function searchGlobal(Request $request)
+    {
         $value = $request->input('_value');
-        $query = XdoArtisan::where('title','like',"%".$value."%")->orwhere('name','like',"%".$value."%")->orwhere('desc','like',"%".$value."%")->orderBy('id','desc');
+        $query = XdoArtisan::where('title', 'like', "%" . $value . "%")->orwhere('name', 'like', "%" . $value . "%")->orwhere('desc', 'like', "%" . $value . "%")->orderBy('id', 'desc');
         $list = $query->paginate(100)->appends($request->all());
-        return view('gs',compact('list'));
+        return view('gs', compact('list'));
     }
 
-
-
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
     public function index()
     {
-        $data = XdoEnv::where("name",'debug')->first();
-        return view('admin::index',compact('data'));
+        $data = XdoEnv::where("name", 'debug')->first();
+        return view('admin::index', compact('data'));
     }
-    protected function logOut(Request $request){
+
+    protected function logOut(Request $request)
+    {
 
         $this->guard()->logoutCurrentDevice();
-
         $request->session()->flush();
-
         return redirect(route('admin.login'));
     }
 
@@ -128,30 +116,33 @@ class AdminController extends \App\Http\Controllers\AdminController
     }
 
 
-    public function menu(){
+    public function menu()
+    {
         Artisan::call('yan:action');
         return redirect(route('admin'));
     }
 
-    public function debugStatus(Request $request){
+    public function debugStatus(Request $request)
+    {
         $status = $request->input('status');
-        $xdo_env = XdoEnv::where("name",'debug')->first();
+        $xdo_env = XdoEnv::where("name", 'debug')->first();
         $xdo_env->status = $status;
         $xdo_env->save();
         return redirect(route('admin'));
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
-        if($request->method() == 'GET'){
+        if ($request->method() == 'GET') {
             return view('admin::register');
         }
 
-        if($request->method() == 'POST'){
+        if ($request->method() == 'POST') {
             $request->validate([
                 'name' => 'required|string|min:3|max:10',
                 'password' => 'required|string|min:3|max:10',
-            ],[
+            ], [
                 'name.required' => '请输入用户名',
                 'name.min' => '用户名最少3字符',
                 'name.max' => '用户名最多10字符',

fcbfc482fe712d715d3c72a1849a9692a9a11b57
yijun.yan <yijun.yan@xueda.com>
2020-12-11 16:50:56 +0800

    操作日志一点点

--- Modules/Curd/Models/XdoLog.php
+++ Modules/Curd/Models/XdoLog.php
@@ -14,56 +14,44 @@ class XdoLog extends XdoBase
     public function getLogDescAttribute(){
         $str = '--';
         $admin = $this->admin_name;
-        $date = $this->created_at;
+        $date = $this->getDateTime();
         $act = $this->act;
         $table = $this->table;
         $t_id = $this->t_id;
         $type = "学生";
         $time = $this->getDateTime($date);
         if($act == 'add'){
-            $str = $admin." 在 ".$date." 往 ".$table."表里 新增了一条".$type."数据";
+            $str = $date." ".$admin." 新增了一条id为".$t_id."的".$type."数据";
         }
         if($act == 'update'){
-            $str = $admin." 在 ".$date." 修改了一条id为 ".$t_id." 的".$type."数据";
+            $str = $date." ".$admin." 修改了一条id为".$t_id."的".$type."数据";
         }
         if($act == 'delete'){
-            $str = $admin." 在 ".$date." 一不小心删了一条id为 ".$t_id." 的".$type;
+            $str = $date." ".$admin." 偷偷删除了一条id为".$t_id."的".$type."数据";
         }
         return $str;
     }
 
     public  function getDateTime(){
 
-        $date = date('H');
+        $date_h = date("H",strtotime($this->created_at));
+        $date = date("m月d日",strtotime($this->created_at));
         $text = "";
 
-        if($date >= 0 && $date < 7){
-
-            $text = "天还没亮，夜猫子，要注意身体哦！ ";
-
-        }else if($date>=7 && $date<12){
-
-            $text = "上午好！今天天气真不错……哈哈哈，不去玩吗？";
-
-        }else if($date >= 12 && $date < 14){
-
-            $text = "中午好！午休时间哦，朋友一定是不习惯午睡的吧？！";
-
-        }else if($date >= 14 && $date < 18){
-
-            $text = "下午茶的时间到了，休息一下吧！ ";
-
-        }else if($date >= 18 && $date < 22){
-
-            $text = "下午茶的时间到了，休息一下吧！ ";
-
-        }else if($date >= 22 && $date < 24){
-
-            $text = "很晚了哦，注意休息呀！";
-
+        if($date_h >= 0 && $date_h < 5){
+            $text = "凌晨";
+        }else if($date_h>=5 && $date_h<8){
+            $text = "早上";
+        }else if($date_h>=8 && $date_h<12){
+            $text = "上午";
+        }else if($date_h >= 12 && $date_h < 14){
+            $text = "中午";
+        }else if($date_h >= 14 && $date_h < 18){
+            $text = "下午";
+        }else if($date_h >= 18 && $date_h < 24){
+            $text = "晚上";
         }
-
-        return $text;
+        return $date."的一个".$text;
 
     }
 }

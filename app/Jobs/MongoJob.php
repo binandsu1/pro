6c73d43417127092ba165fc5297337cd4baef8d5
yijun.yan <yijun.yan@xueda.com>
2021-01-20 18:31:45 +0800

    mongo

---
+++ app/Jobs/MongoJob.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Modules\Mongo\Models\MongData;
+
+class MongoJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($i)
+    {
+        $this->connection = 'rabbitmq';
+        $this->queue = 'MongJob';
+        $this->i = $i;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $add = [];
+        $add["name"] = "数据-".$this->i;
+        $add["roundA"] = rand(100000, 999999);
+        $add["roundB"] = rand(100000, 999999);
+        $add["roundC"] = rand(100000, 999999);
+        $add["roundD"] = rand(100000, 999999);
+        $add["roundE"] = rand(100000, 999999);
+        $add["roundF"] = rand(100000, 999999);
+        $add["roundG"] = rand(100000, 999999);
+        $add["roundH"] = rand(100000, 999999);
+        $model = new MongData();
+        $model->fill($add);
+        $model->save();
+    }
+}

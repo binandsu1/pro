402a36f363fefc2ddf481525838d35a039bd1043
yijun.yan <yijun.yan@xueda.com>
2021-03-11 16:37:47 +0800

    aaa

--- Modules/Curd/Resources/views/admin/demo1.blade.php
+++ Modules/Curd/Resources/views/admin/demo1.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            数据列表 - 横111
+            数据列表 - 横2
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Laravel </a></li>

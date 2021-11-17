a6d157b778e8d648e9298971649f74c5d976d7bd
yijun.yan <yijun.yan@xueda.com>
2021-11-17 16:20:15 +0800

    ss

---
+++ app/Http/Controllers/wxtestController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use EasyWeChat\Factory;
+use Illuminate\Http\Request;
+
+class wxtestController extends Controller
+{
+
+    function index(){
+
+        $options = [
+            'app_id'    => 'wx1647b3429377748f',
+            'secret'    => '7c837f2ff2887845c13558742232a43d',
+            'token'     => 'pasdpasdasd22',
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log',
+            ],
+            // ...
+        ];
+
+        $app = Factory::officialAccount($options);
+
+        $server = $app->server;
+        $user = $app->user;
+
+        $server->push(function($message) use ($user) {
+            $fromUser = $user->get($message['FromUserName']);
+
+            return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
+        });
+
+        $server->serve()->send();
+    }
+
+//
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/wx', [\App\Http\Controllers\wxtestController::class,'index'])->name('wx');
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
